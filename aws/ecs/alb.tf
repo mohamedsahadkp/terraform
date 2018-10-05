f0b1af239acb00c463619e9da5eea36fe379569e
Mohamed Sahad K P <mohamed.sahad@experionglobal.com>
2018-10-05 15:12:23 +0530

    added alb-tg in ecs

--- aws/ecs/alb.tf
+++ aws/ecs/alb.tf
@@ -7,36 +7,3 @@ resource "aws_alb" "tf_alb" {
       Name = "${var.project_name}-${var.project_env}-alb"
     }
 }
-
-resource "aws_alb_target_group" "tf_tg" {
-    name                = "${var.project_name}-${var.project_env}-tg"
-    port                = "80"
-    protocol            = "HTTP"
-    vpc_id              = "${aws_vpc.tf_vpc.id}"
-
-    health_check {
-        healthy_threshold   = "5"
-        unhealthy_threshold = "2"
-        interval            = "30"
-        matcher             = "200"
-        path                = "/"
-        port                = "traffic-port"
-        protocol            = "HTTP"
-        timeout             = "5"
-    }
-
-    tags {
-      Name = "${var.project_name}-${var.project_env}-tg"
-    }
-}
-
-resource "aws_alb_listener" "tf_alb_listener" {
-    load_balancer_arn = "${aws_alb.tf_alb.arn}"
-    port              = "80"
-    protocol          = "HTTP"
-
-    default_action {
-        target_group_arn = "${aws_alb_target_group.tf_tg.arn}"
-        type             = "forward"
-    }
-}
--- aws/ecs/task-definition.tf
+++ aws/ecs/task-definition.tf
@@ -1,35 +1,31 @@
-data "aws_ecs_task_definition" "tf_ecs_td" {
-  task_definition = "${aws_ecs_task_definition.wordpress.family}"
-}
-
 resource "aws_ecs_task_definition" "tf_ecs_td" {
-    family                = "darisni-academy-api"
-    container_definitions = <<DEFINITION
-        [
-          {
-            "name": "wordpress",
-            "links": [
-              "mysql"
-            ],
-            "image": "wordpress",
-            "image": "129800272058.dkr.ecr.eu-west-2.amazonaws.com/darisni-academy-api-admin:uat",
-              "logConfiguration": {
-                "logDriver": "awslogs",
-                "options": {
-                  "awslogs-group": "/app/darisni/academy/api-uat/admin",
-                  "awslogs-region": "eu-west-2"
-                }
-              },
-            "essential": true,
-            "portMappings": [
-              {
-                "containerPort": 80,
-                "hostPort": 0
-              }
-            ],
-            "memory": 500,
-            "cpu": 100
-          }
-        ]
-    DEFINITION
+  family                = "${var.project_name}-${var.project_env}-ms1"
+  container_definitions = <<DEFINITION
+      [
+        {
+          "name": "wordpress",
+          "links": [
+            "mysql"
+          ],
+          "image": "wordpress",
+          "image": "129800272058.dkr.ecr.eu-west-2.amazonaws.com/darisni-academy-api-admin:uat",
+          "logConfiguration": {
+            "logDriver": "awslogs",
+            "options": {
+              "awslogs-group": "/app/darisni/academy/api-uat/admin",
+              "awslogs-region": "eu-west-2"
+            }
+          },
+          "essential": true,
+          "portMappings": [
+            {
+              "containerPort": 80,
+              "hostPort": 0
+            }
+          ],
+          "memory": 500,
+          "cpu": 100
+        }
+      ]
+  DEFINITION
 }
