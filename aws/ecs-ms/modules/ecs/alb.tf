7f62cdd9a824e4c3cda202fef2d17a111df0d77d
Mohamed Sahad KP <mohamedsahadkp@gmail.com>
2019-05-06 10:34:40 +0530

    alb,alb-sg, cluster into single module as main.tf

--- aws/ecs-ms/modules/ecs/alb.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_alb" "tf_alb" {
-  name            = "${var.project_name}-${var.project_environment}-alb"
-  subnets         = ["${var.public_subnet_1a}", "${var.public_subnet_1b}"]
-  security_groups = ["${aws_security_group.tf_alb_sg.id}"]
-}
