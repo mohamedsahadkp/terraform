e447e57fda6bb9f89897f56abee473120bb1581c
Mohamed Sahad K P <mohamedsahadkp@gmail.com>
2021-02-20 22:52:36 +0530

    added alb listener and alb tg and tg attachment

--- monolithic-app/modules/api/alb.tf
+++ monolithic-app/modules/api/alb.tf
@@ -17,3 +17,36 @@ resource "aws_lb" "alb" {
     var.project.resource_tags, map("Name", "${var.project.name}-${var.project.environment}-api-server-alb")
   )
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
+
+resource "aws_lb_target_group" "ip-example" {
+  name        = "tf-example-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.test.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.test.id
+  port             = 80
+}
