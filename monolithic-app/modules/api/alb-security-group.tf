35d28f77301b962f32771e9c53e94251207c1512
Mohamed Sahad <mohamedsahadkp>
2021-03-05 13:49:49 +0530

    added dynamic block in alb sg

--- monolithic-app/modules/api/alb-security-group.tf
+++ monolithic-app/modules/api/alb-security-group.tf
@@ -1,21 +1,29 @@
+variable "alb_security_group_list" {
+  type    = list
+  default = [443, 80]
+}
+
 resource "aws_security_group" "alb_security_group" {
   name        = "${var.project.name}-${var.project.environment}-alb-sg"
   description = "${var.project.name}-${var.project.environment}-alb-sg"
   vpc_id      = var.vpc
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = var.alb_security_group_list
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   egress {
     from_port   = 0
